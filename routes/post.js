c5f0bfcf5bcea6de0834cca5fec53682d689f7a3
Safiul islam <45520613+ishafiul@users.noreply.github.com>
2022-03-09 17:56:14 +0600

    access header remove

--- routes/post.js
+++ routes/post.js
@@ -49,7 +49,7 @@ router.post('/create', upload.single('postImg'),async (req,res)=>{
 
 //get
 router.get('/',async (req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    
     const username = req.query.user;
     try {
         let post;
