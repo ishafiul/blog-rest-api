175c0e0b2f665ca48888424a751ccbba9e566029
shafiul islam <shafiulislam20@gmail.com>
2022-08-30 21:33:44 +0600

    feat: basic game finish

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, {
     cors: {
-        origin: "*",
+        origin: "https://shafi-org.github.io/3x/",
         methods: ["GET", "POST"],
     },
 });
